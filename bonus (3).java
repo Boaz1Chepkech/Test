4d9e25304c2dcf35a4da6a1c03347ebdce0ff3e7
Boaz1Chepkech <boazchepkech@gmail.com>
2025-01-31 12:52:41 +0300

    updates

--- bonus (3).java
+++ bonus (3).java
@@ -1,3 +1,27 @@
+// // main class
+// class Main{
+//     public static void main(String[] args){
+//         int salary=500000;
+//         int yearofservice=7;
+//         int bonus=0;
+        
+//         //find the bonus
+//         if(yearofservice>10){
+//             bonus=12;
+//         }
+//         else if(yearofservice>=6 && yearofservice<=10){
+//             bonus=10;
+//         }
+//         else{
+//             bonus=8;
+//     }
+    
+//     //compute bonus salary
+//     int bonussalary=salary*bonus/100;
+//     //print the output bonus
+//     System.out.println("Your bonus salary is" +bonussalary);
+// }}
+
 // main class
 class Main{
     public static void main(String[] args){
